8ce4925a9f9619ec4f34e840fccd2dc1bd6bf142
Luna <ljx_6717@126.com>
2020-06-07 12:44:46 +0800

    重构 NumberPadSection.tsx

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,87 +1,54 @@
-import styled from 'styled-components';
 import React, {useState} from 'react';
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {generateOutput} from './NumberPadSection/generateOutput';
 
-const Wrapper = styled.section`
-  display:flex;
-  flex-direction: column;
-  > .output{
-    background:white;
-    font-size:36px;
-    line-height:72px;
-    text-align:right;
-    padding:0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-                inset 0 5px  5px -5px rgba(0,0,0,0.25);
-  }
-  > .pad {
-    > button{float:left;width:25%;height:64px;
-      &.ok{height:128px; float:right;}
-      &.zero{width:50% };
-      &:nth-child(1){background: #f2f2f2;}
-      &:nth-child(2),&:nth-child(5){background: #e0e0e0;}
-      &:nth-child(3),&:nth-child(6),&:nth-child(9){background: #d3d3d3;}
-      &:nth-child(4),&:nth-child(7),&:nth-child(10){background:#C1C1C1;}
-      &:nth-child(8),&:nth-child(11),&:nth-child(13){background: #b8b8b8;}
-      &:nth-child(12){background: #9A9A9A;}
-      &:nth-child(14){background:#a9a9a9;}
+
+const NumberPadSection: React.FC = () => {
+  const [output, _setOutput] = useState('0')
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16)
+    } else if (output.length === 0) {
+      output = '0'
     }
-  }
-`
-const NumberPadSection: React.FC =()=>{
-  const [output,setOutput]=useState('0')
-  const onClickButtonWrapper = (e:React.MouseEvent)=>{
+    _setOutput(output);
+  };
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
-    if(text===null){return}
-    switch(text){
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        console.log('text');
-        break;
-      case'.':
-        console.log('点');
-        break;
-      case'删除':
-        console.log('删除');
-        break;
-      case'清空':
-        console.log('清空');
-        break;
-      case'OK':
-        console.log('确认');
-        break;
+    if (text === null) {return;}
+    if (text === 'OK') {
+      //todo
+      return;
     }
+    if ('0123456789.'.split('').concat(['删除', '清空']).indexOf(text) >= 0) {
+      setOutput(generateOutput(text, output));
 
-  }
-  return (
-    <Wrapper>
-      <div className="output">
-        {output}
-      </div>
-      <div className="pad clearfix" onClick = {onClickButtonWrapper}>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button className="ok">OK</button>
-        <button className="zero">0</button>
-        <button className="dot">.</button>
-      </div>
-    </Wrapper>
-  )
+    }
+    ;
+    }
+    return (
+      <Wrapper>
+        <div className="output">
+          {output}
+        </div>
+        <div className="pad clearfix" onClick={onClickButtonWrapper}>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
+          <button className="dot">.</button>
+        </div>
+      </Wrapper>
+    )
+
 }
-export {NumberPadSection}
+export {NumberPadSection};
