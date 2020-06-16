8065a8e8f6c80293bd6557a5ab672d603e2e8ab1
Luna <ljx_6717@126.com>
2020-06-16 09:16:10 +0800

    Tag.tsx的样式完成

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,8 +8,8 @@ const Label = styled.label`
      >input{
        display:block;
        width: 100%;
-       height:72px;
-        background:none;
+       height:44px;
+       background:none;
        border:none;
      }
 `
