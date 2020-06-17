435fdb04d5bd5b0f19033dbc4014919dce4e2981
Luna <ljx_6717@126.com>
2020-06-17 14:19:44 +0800

    删除 tag 实现

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -26,8 +26,14 @@ const useTags = ()=>{//封装一个自定义HOOK
     const tagsClone = JSON.parse(JSON.stringify(tags));
     tagsClone.splice(index,1,{id:id,name:obj.name})
     setTags(tagsClone);
+  };
+  const deleteTag = (id:number)=>{
+    const index = findTagIndex(id);
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    tagsClone.splice(index,1)
+    setTags(tagsClone)
   }
-  return{tags, setTags,findTag,updateTag,findTagIndex};
+  return{tags,setTags,findTag,updateTag,findTagIndex,deleteTag};
 }
 
 export {useTags}
