a14052913f64adc50df58ef0e57bafefa56b1256
Luna <ljx_6717@126.com>
2020-06-21 14:07:22 +0800

    检查输入

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -13,6 +13,11 @@ export const useRecords = ()=>{
   useEffect(()=>{setRecords(JSON.parse(window.localStorage.getItem('records')||'[]'))},[])
   useUpdate(()=>{window.localStorage.setItem('records',JSON.stringify(records))},[records])
   const addRecord = (newRecord:newRecordItem)=>{
+    if(newRecord.amount <= 0){alert('请输入金额');return false;}
+    if(newRecord.tagIds.length === 0){
+      alert('请输入标签')
+      return false
+    }
     const record = {...newRecord,createAt:(new Date()).toISOString()}
     setRecords([...records,record])}
   return{records, addRecord}
