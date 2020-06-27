f897090565e676c07fb57b047c93c7f66c683046
Luna <ljx_6717@126.com>
2020-06-27 14:52:01 +0800

    重构useUpdate，消除所有警告

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -11,7 +11,7 @@ type newRecordItem = Omit<RecordItem,'createAt'>
 export const useRecords = ()=>{
   const [records,setRecords] = useState<RecordItem[]>([]);
   useEffect(()=>{setRecords(JSON.parse(window.localStorage.getItem('records')||'[]'))},[])
-  useUpdate(()=>{window.localStorage.setItem('records',JSON.stringify(records))},[records])
+  useUpdate(()=>{window.localStorage.setItem('records',JSON.stringify(records))},records)
   const addRecord = (newRecord:newRecordItem)=>{
     if(newRecord.amount <= 0){alert('请输入金额');return false;}
     if(newRecord.tagIds.length === 0){
--- src/hooks/useUpdate.tsx
+++ src/hooks/useUpdate.tsx
@@ -1,9 +1,9 @@
 import {useEffect, useRef} from 'react';
 
-const useUpdate = (fn:()=>void,deps:any[])=>{
+const useUpdate = (fn:()=>void,dependency:any[])=>{
   const count = useRef(0);
   useEffect(()=>{count.current += 1});
-  useEffect(()=>{if(count.current > 1){fn()}},deps)
+  useEffect(()=>{if(count.current > 1){fn()}},[fn, dependency])
 }
 
 export {useUpdate}
