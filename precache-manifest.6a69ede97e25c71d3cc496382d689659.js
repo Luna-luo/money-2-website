9df489189dc88db945115dca44ce7e64e5ed774d
luojinxu <7752998+LunaLuo@user.noreply.gitee.com>
2020-06-30 16:27:28 +0800

    deploy

---
+++ precache-manifest.6a69ede97e25c71d3cc496382d689659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e26013a6b4431d1d407b200e8a76da",
+    "url": "/money-2-website/index.html"
+  },
+  {
+    "revision": "89b679bd27824cb815db",
+    "url": "/money-2-website/static/css/main.7990ed03.chunk.css"
+  },
+  {
+    "revision": "ea45e7c00438dbc164c1",
+    "url": "/money-2-website/static/js/2.34d77c41.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/money-2-website/static/js/2.34d77c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89b679bd27824cb815db",
+    "url": "/money-2-website/static/js/main.d44e35ec.chunk.js"
+  },
+  {
+    "revision": "99126c8857f55c78753e",
+    "url": "/money-2-website/static/js/runtime-main.a38ab08f.js"
+  }
+]);
