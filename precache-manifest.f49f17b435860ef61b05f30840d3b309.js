cb1e991d149d6b01815032d1514b6cc18443e585
luojinxu <7752998+LunaLuo@user.noreply.gitee.com>
2020-08-05 08:20:02 +0800

    debug

---
+++ precache-manifest.f49f17b435860ef61b05f30840d3b309.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5393aef54f80e6b60dd6854f10319ea",
+    "url": "/money-2-website/index.html"
+  },
+  {
+    "revision": "05f7d9f6be9aaa986290",
+    "url": "/money-2-website/static/css/main.7990ed03.chunk.css"
+  },
+  {
+    "revision": "ea45e7c00438dbc164c1",
+    "url": "/money-2-website/static/js/2.34d77c41.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/money-2-website/static/js/2.34d77c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f7d9f6be9aaa986290",
+    "url": "/money-2-website/static/js/main.ec96050c.chunk.js"
+  },
+  {
+    "revision": "99126c8857f55c78753e",
+    "url": "/money-2-website/static/js/runtime-main.a38ab08f.js"
+  }
+]);
