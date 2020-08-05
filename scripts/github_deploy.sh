b1ac275d2ba2dfde0ca2528835f236b4018d3e5b
luojinxu <7752998+LunaLuo@user.noreply.gitee.com>
2020-08-05 08:26:40 +0800

    添加文件

--- scripts/github_deploy.sh
+++ scripts/github_deploy.sh
@@ -4,7 +4,7 @@ yarn build &&
 cd build &&
 git init &&
 git add . &&
-git commit -m 'deploy' &&
+git commit -m 'debug' &&
 git remote add origin git@github.com:Luna-luo/money-2-website.git &&
 git push -u origin master -f
 cd -
